a4b5aa0e8ce867b7dabdecba579f10d7f93cee09
pedrovt <pedro.teix@ua.pt>
2018-10-09 15:42:39 +0100

    [mksosfs/resetBlocks] Added first working version
    Once again, ONLY tested for a disk with 1000 blocks! Refactor of the code soon

--- sofs18/src/work_src/work_mksofs/work_mksofs_RC.cpp
+++ sofs18/src/work_src/work_mksofs/work_mksofs_RC.cpp
@@ -15,9 +15,23 @@ namespace sofs18
         void resetBlocks(uint32_t first_block, uint32_t cnt)
         {
             soProbe(607, "%s(%u, %u)\n", __FUNCTION__, first_block, cnt);
+            printf("\t[PVT/DEBUG] Using my version of resetBlocks!!!!");
             
+            uint32_t size = BlockSize / sizeof(uint32_t);
+            uint32_t array[size];  // Macro ReferencesPerBlock wouldn't be clear
+            for (uint32_t i = 0; i < size; i++)
+            {
+                array[i] = 0;
+                //printf("\t[PVT/DEBUG] array[%d]=%d\n", i, array[i]);
+            }
+            
+            for (; first_block < cnt; first_block++) 
+            {
+                soWriteRawBlock(first_block, &array);
+            }
+
             /* change the following line by your code */
-            bin::resetBlocks(first_block, cnt);
+            //bin::resetBlocks(first_block, cnt);
         }
 
     };
