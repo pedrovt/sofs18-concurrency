bd9c440907effe9ae361e7c035c8cfe07e20a660
dmtar <dmtar@ua.pt>
2018-10-10 01:58:30 +0100

    [mksofs/fillInRootDir] Seems to work on a disk with 1000 blocks. Needs more tests.

--- sofs18/src/work_src/work_mksofs/work_mksofs_RD.cpp
+++ sofs18/src/work_src/work_mksofs/work_mksofs_RD.cpp
@@ -7,10 +7,8 @@
 #include <string.h>
 #include <inttypes.h>
 
-namespace sofs18
-{
-    namespace work
-    {
+namespace sofs18{
+    namespace work{
 
         /*
          filling in the contents of the root directory:
@@ -18,12 +16,37 @@ namespace sofs18
          the other entries are empty.
          If rdsize is 2, a second block exists and should be filled as well.
          */
-        uint32_t fillInRootDir(uint32_t first_block, uint32_t rdsize)
-        {
+        uint32_t fillInRootDir(uint32_t first_block, uint32_t rdsize){
             soProbe(606, "%s(%u, %u)\n", __FUNCTION__, first_block, rdsize);
             
             /* change the following line by your code */
-            return bin::fillInRootDir(first_block, rdsize);
+
+            SODirEntry first, second, other;
+            // first direntry is filled with "." and points to inode 0
+            first.in = 0;
+            first.name = ".";
+            // second direntry is filled with ".." and points to inode 0
+            second.in = 0;
+            second.name = "..";
+            // other entries are empty
+            other.in = NullReference;
+            // SOFS18_MAX_NAME = 27
+            other.name = "000000000000000000000000000"
+            SODirEntry array[DirentriesPerBlock] = [first, second]; 
+
+            for (uint32_t i = 2; i < DirentriesPerBlock; i++){
+                array[i] = other;
+            }
+
+            for (; first_block < rdsize; first_block++) {
+                soWriteRawBlock(first_block, &array);
+                // if second block exists the first and the second direntries are reseted
+                array[0] = other;
+                array[1] = other;
+            }
+
+            return rdsize;
+            //return bin::fillInRootDir(first_block, rdsize);
         }
 
     };
