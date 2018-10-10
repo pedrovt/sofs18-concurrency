e9f2c1e93dc0b0b6a4b677d2b03ed2d3b1ccf56d
Gabriel <augustosilva@ua.pt>
2018-10-10 12:05:53 +0100

    Mudei a abordagem. Está mais correto agora

--- sofs18/src/work_src/work_mksofs/work_mksofs_FBLT.cpp
+++ sofs18/src/work_src/work_mksofs/work_mksofs_FBLT.cpp
@@ -18,37 +18,38 @@ namespace sofs18
         uint32_t fillInFreeBlockListTable(uint32_t first_block, uint32_t btotal, uint32_t rdsize)
         {
             soProbe(605, "%s(%u, %u, %u)\n", __FUNCTION__, first_block, btotal, rdsize);
-          		
+			std::cout << "Our version of fillInFreeBlockListTable";			          		
 			uint32_t n_blocks = roundNBlocksUp(btotal);
-			uint32_t table [n_blocks * ReferencesPerBlock];
-			for(uint32_t j = rdsize; j < btotal; j++) {
-				table[j - rdsize] = j;
+			uint32_t block[ReferencesPerBlock];
+			uint32_t last_reference_number = rdsize; 
+            for(uint32_t i = 0; i < n_blocks-1; i++) {
+				for(uint32_t j=0; j < ReferencesPerBlock; j++) {
+					block[j] = last_reference_number++;	
+				}
+				soWriteRawBlock(first_block + i, &block);	
 			}
-			//Fill all blocks but the last because the last may have NullReferences
-			for(uint32_t i = 0; i < n_blocks-1; i++) {
-				soWriteRawBlock(first_block + i, &table[128*i]);
-			}
-
-			//the address of the last block
+			
 			uint32_t last_block = first_block + n_blocks - 1;
-			//table with the last block's references
-			uint32_t table_last_block [ReferencesPerBlock];
-			//amount of references that exist in last block
 			uint32_t remainder = btotal % ReferencesPerBlock-1;
-
-			for(uint32_t k = 0; k < remainder; k++) {
-				//this sum makes us continue form the last reference
-				table_last_block[k] = k + table[(n_blocks-1)*128];
+		
+	
+			if(btotal%ReferencesPerBlock != 0) {
+				for(uint32_t i=0; i<ReferencesPerBlock; i++) {
+					if(i < remainder) {
+						block[i] = last_reference_number++;
+					}else {
+						block[i] = NullReference;
+					}	
+				}
+				soWriteRawBlock(last_block, &block);
+			}else {
+				for(uint32_t i=0; i < ReferencesPerBlock; i++) {
+					block[i] = last_reference_number++;	
+				}
+				soWriteRawBlock(last_block, &block);	
 			}
-			//fill the rest of the array with NullReference
-			for(uint32_t l = remainder; l < ReferencesPerBlock; l++) {
-				table_last_block[l] = NullReference;
-			}
-			//write the last block
-			soWriteRawBlock(last_block, &table_last_block);
-
 			return n_blocks;
-            //return bin::fillInFreeBlockListTable(first_block, btotal, rdsize);
+			//return bin::fillInFreeBlockListTable(first_block, btotal, rdsize);
 			
         }
 
