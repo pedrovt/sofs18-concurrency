74530f8e7355d1dbedd41f79830a7c9dd38546f9
andremourato@ua.pt <andremourato@ua.pt>
2018-10-13 00:21:59 +0100

    Added more documentation to the function

--- sofs18/src/work_src/work_mksofs/work_mksofs_FILT.cpp
+++ sofs18/src/work_src/work_mksofs/work_mksofs_FILT.cpp
@@ -11,8 +11,13 @@ namespace sofs18
 {
     namespace work
     {
-
+    	//Determines the number of blocks necessary, given the number of total inodes and the number of references per block
     	uint32_t determineNumBlocks(uint32_t total, uint32_t rpb){ return total/rpb + (total%rpb != 0 ? 1 : 0); }
+		/*
+	    	[in]	first_block	physical number of the first block used by the table
+			[in]	itotal	the total number of inodes
+			[out]   The number of blocks occupied by the table
+		*/
         uint32_t fillInFreeInodeListTable(uint32_t first_block, uint32_t itotal)
         {
             soProbe(603, "%s(%u, %u)\n", __FUNCTION__, first_block, itotal);
@@ -24,14 +29,19 @@ namespace sofs18
             uint32_t num = ReferencesPerBlock;
             uint32_t index = 1; //Starts with 1, because the first inode has been used for the root directory
             for(uint32_t i = first_block; i <= last_block; i++){
+            	/* Checks if the current block is the last. If so, calculates how many references must be filled.
+            	   Else it just fills all ReferencesPerBlock references */
             	if(i == last_block) num = itotal - ReferencesPerBlock * (num_blocks-1);
             	uint32_t j = 0;
+            	/*Fills the blocks dedicated to the Free INode Table List*/
             	for(; j < num; j++){
             		block[j] = index++;
             	}
+            	/*Fills the remaining references with NullReference*/
             	for(uint32_t k = j; k < ReferencesPerBlock; k++){
             		block[j++] = NullReference;
             	}
+            	/*Writes each buffer to the corresponding block*/
             	sofs18::soWriteRawBlock(i,&block);
             }
             return num_blocks;
