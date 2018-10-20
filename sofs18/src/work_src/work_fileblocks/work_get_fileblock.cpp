1d30f9759604011e7a59641745f4469eb8fd6766
pedrovt <pedro.teix@ua.pt>
2018-10-21 00:18:44 +0100

    [fileblocks/get_fileblock] Added part of implementation. NOT finished!
    Don't use it!

--- sofs18/src/work_src/work_fileblocks/work_get_fileblock.cpp
+++ sofs18/src/work_src/work_fileblocks/work_get_fileblock.cpp
@@ -6,53 +6,90 @@
 
 #include <errno.h>
 
+#include <math.h>
+
 namespace sofs18
 {
     namespace work
     {
 
         /* ********************************************************* */
-
-#if false
         static uint32_t soGetIndirectFileBlock(SOInode * ip, uint32_t fbn);
         static uint32_t soGetDoubleIndirectFileBlock(SOInode * ip, uint32_t fbn);
-#endif
 
         /* ********************************************************* */
+        // *    -> Tested
+        // ?    -> Under Testing
+        // !    -> Bugs
+        // TODO -> Not implemented
 
         uint32_t soGetFileBlock(int ih, uint32_t fbn)
         {
             soProbe(301, "%s(%d, %u)\n", __FUNCTION__, ih, fbn);
+            printf("\tUsing work version\n");
+
+            SOInode* inode = soITGetInodePointer(ih);
+
+            /* Determine if the block is in direct field, indirect field or
+            double indirect field */
+            
+            /* direct, d */ 
+            if (fbn < N_DIRECT)         
+            {
+                return inode -> d[fbn];
+            }
+            
+            /* indirect, i */
+            uint32_t maxIndirectIndex = N_INDIRECT * ReferencesPerBlock + N_DIRECT - 1;
+            if (fbn < maxIndirectIndex) 
+            {
+                return soGetIndirectFileBlock(inode, fbn - N_DIRECT);
+            }
+
+            /* double indirect, i2 */  //--> here? 
+            else if (fbn < ReferencesPerBlock*ReferencesPerBlock + maxIndirectIndex)    
+            {
+                return soGetDoubleIndirectFileBlock(inode, fbn - N_INDIRECT * 2 - N_DIRECT);
+            }
+
+            /* error */
+            else                        
+            {
+                return NullReference;
+            }
 
             /* change the following line by your code */
-            return bin::soGetFileBlock(ih, fbn);
+            // return bin::soGetFileBlock(ih, fbn);
         }
 
         /* ********************************************************* */
-
-#if false
         static uint32_t soGetIndirectFileBlock(SOInode * ip, uint32_t afbn)
-        {
+        {   
             soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
-
+            printf("\tIndirect. Trying to find index %d\n", afbn);
+            
+            /* get i1[0]/i1[1] */
+            uint32_t* i = &ip->i1[afbn / ReferencesPerBlock];
+            
+            /* get position in selected i1 */
+            return i[afbn % ReferencesPerBlock];
+            
             /* change the following two lines by your code */
-            throw SOException(ENOSYS, __FUNCTION__); 
-            return 0;
+            //throw SOException(ENOSYS, __FUNCTION__); 
+            //return 0;
         }
-#endif
 
         /* ********************************************************* */
-
-#if false
         static uint32_t soGetDoubleIndirectFileBlock(SOInode * ip, uint32_t afbn)
         {
             soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
+            printf("\nDouble Indirect. Trying to find index %d\n", afbn);
+
 
             /* change the following two lines by your code */
             throw SOException(ENOSYS, __FUNCTION__); 
             return 0;
         }
-#endif
 
     };
 
--- sofs18/src/work_src/work_freelists/work_replenish_ircache.cpp
+++ sofs18/src/work_src/work_freelists/work_replenish_ircache.cpp
@@ -21,17 +21,10 @@ namespace sofs18
 
         using namespace std;
 
-        /*  Considerations
-                Nothing should be done if the retrieval cache is not empty.
-                The insertion cache should only be used if the free inode list table is empty.
-                Only a single block should be processed, even if it is not enough to fulfill the retrieval cache.
-                The block to be processes is the one pointed to by the filt_head field of the superblock.
-            Steps
-                1. If cache is not empty (ie idx != size of cache), return
-                2. Else, if table is not free (ie head != tail), copy 1 block (max) in the end of the cache and update idx
-                3. Else, if insertion cache has elements, copy 1 block (max) from insertion cache and update idx
-                4. Else, no Inodes are free, throw an exception [see man errno]
-        */
+        // *    -> Tested
+        // ?    -> Under Testing
+        // !    -> Bugs
+        // TODO -> Not implemented
         void soReplenishIRCache(void)
         {
             soProbe(403, "%s()\n", __FUNCTION__);
@@ -40,19 +33,19 @@ namespace sofs18
             SOSuperBlock* sb = soSBGetPointer();
             SOInodeReferenceCache ircache = sb -> ircache;
 
-            /* If cache is not empty (idx != size of cache), 
-            there's no need to replenish the cache! ------------------------- */
+            //? If cache is not empty (idx != size of cache), 
+            // there's no need to replenish the cache! ---------------------- */
             if (ircache.idx != INODE_REFERENCE_CACHE_SIZE - 1) {
                 return;
             }
 
-            /* If no inodes are free, can't replenish the cache! ------------ */
+            //? If no inodes are free, can't replenish the cache! ------------ */
             // Considering no free inodes as "No space left on device"
             if (sb->ifree == 0) {
                 throw SOException(ENOSPC, __FUNCTION__);
             }
            
-            /* If FILT is not empty (ie head != tail), copy from it --------- */
+            // If FILT is not empty (ie head != tail), copy from it --------- */
             if (sb -> filt_head != sb -> filt_tail) {
                 // Number of the block
                 uint32_t numBlock    = (sb -> filt_head) / ReferencesPerBlock; 
@@ -109,7 +102,7 @@ namespace sofs18
                 }
             }
 
-            /* Else, if insertion cache is not empty, copy from it ---------- */
+            //? Else, if insertion cache is not empty, copy from it ---------- */
             SOInodeReferenceCache iicache = sb->iicache;
             if (iicache.idx != 0)
             {
@@ -137,7 +130,7 @@ namespace sofs18
             soFILTCloseBlock();
             soSBSave();
 
-            /* To use the given binary version ------------------------------ */
+            // To use the given binary version ------------------------------ */
             //bin::soReplenishIRCache();
         }
 
