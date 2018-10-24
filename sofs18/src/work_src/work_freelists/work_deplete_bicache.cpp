089a123163fe2718c31262e92f94ecdecf95844f
gabriel <augustosilva@ua.pt>
2018-10-24 10:02:38 +0100

    freelists/deplete_bicache

--- sofs18/src/work_src/work_freelists/work_deplete_bicache.cpp
+++ sofs18/src/work_src/work_freelists/work_deplete_bicache.cpp
@@ -24,10 +24,35 @@ namespace sofs18
         /* only fill the current block to its end */
         void soDepleteBICache(void)
         {
-            soProbe(444, "%s()\n", __FUNCTION__);
+		soProbe(444, "%s()\n", __FUNCTION__);
+		
+		soSUperBlock *sb = soSBGetPointer();
+		soBlockReferenceCache bicache = sb->bicache;
 
+		uint32_t block_number = sb.fblt_tail / ReferencesPerBlock;
+		uint32_t position_in_block = sb.fblt_tail % ReferencesPerBlock;
+		uint32_t *block = soFBLTOpenBlock(block_number);
+
+		if(bicache.idx > 0) {
+			if((ReferencesPerBlock - position_in_block) < bicache.idx) {
+				uint32_t numBytes = (ReferencesPerBlock - position_in_block) * sizeof(uint32_t);
+				//copy references from cache to the FBLT
+				memcpy(&block[position_in_block], &bicache, numBytes);
+				//delete those references from the cache
+				memset(&bicache, NullReference, numBytes);
+			}else {
+				uint32_t numBytes = bicache.idx * sizeof(uint32_t);
+				//copy references from the cache to the FBLT
+				memcpy(&block[position_in_block], &bicache, numBytes);
+				//delete those references from the cache
+				memset(&bicache, NullReference, numBytes);
+			}
+		}
+		
+		
+		soSBSave();
             /* change the following line by your code */
-            bin::soDepleteBICache();
+            //bin::soDepleteBICache();
         }
 
     };
