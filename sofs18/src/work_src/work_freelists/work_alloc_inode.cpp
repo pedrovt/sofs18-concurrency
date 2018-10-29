4e1887607ce5bc044c1cb40304bfc185394e4654
andremourato@ua.pt <andremourato@ua.pt>
2018-10-29 15:18:36 +0000

    Added Alloc INode method tested

--- sofs18/src/work_src/work_freelists/work_alloc_inode.cpp
+++ sofs18/src/work_src/work_freelists/work_alloc_inode.cpp
@@ -25,13 +25,61 @@ namespace sofs18
 {
     namespace work
     {
+    	/*
 
+		Allocate a free inode.
+		An inode reference is retrieved from inode retrieval cache and the inode is initialized.
+		If the cache is empty, it has to be replenished before the retrieval takes place.
+		Parameters
+		    [in]	type	the inode type (it must represent either a file, or a directory, or a symbolic link)
+		Returns
+		    the number (reference) of the inode allocated 
+
+		*/
         uint32_t soAllocInode(uint32_t type)
         {
             soProbe(401, "%s(%x)\n", __FUNCTION__, type);
+            //return bin::soAllocInode(type);
+            //Retrieve the inode reference from the inode retrieval cache
+            soSBOpen();
+            SOSuperBlock* sb = soSBGetPointer();
+            //Checks for free inodes
+            if(sb->ifree <= 0){
+            	throw SOException(ENOSPC,__FUNCTION__);
+            }
+            //If the cache is empty, it has to be replenished before the retrieval takes place.
+            if(sb->ircache.idx == INODE_REFERENCE_CACHE_SIZE) {
+            	soReplenishIRCache();
+                soSBOpen();
+                //Updates with the new information after the replenish
+                sb = soSBGetPointer();
+                sb->ircache = sb->ircache;
+            }
+            uint32_t retrieved_node = sb->ircache.ref[sb->ircache.idx];
+            int handler = soITOpenInode(retrieved_node);
+            //soITCheckHandler(handler,__FUNCTION__); --> apparently doesn't exist. But it's in the documentation
+            sb->ircache.ref[sb->ircache.idx] = NullReference;
+            sb->ircache.idx += 1;
+            sb->ifree -= 1;
+            SOInode* inode = soITGetInodePointer(handler);
+            //INode is initialized
+			inode->mode = uint16_t(type);
+			inode->lnkcnt = 0;
+			inode->owner = getuid();
+			inode->group = getgid();
+			inode->size = 0;
+			inode->blkcnt = 0;
+			inode->atime = inode->mtime = inode->ctime = time(NULL);
+            for(int i = 0; i < N_DIRECT; i++) inode->d[i] = NullReference;
+            for(int i = 0; i < N_INDIRECT; i++) inode->i1[i] = NullReference;
+            for(int i = 0; i < N_DOUBLE_INDIRECT; i++) inode->i2[i] = NullReference;
 
-            /* change the following line by your code */
-            return bin::soAllocInode(type);
+			//Save inode
+			soITSaveInode(handler);
+			soITCloseInode(handler);
+            //Save SB
+			soSBSave();
+            return retrieved_node;
         }
 
     };
