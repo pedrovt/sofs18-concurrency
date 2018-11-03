e33f24ff6c9e1e1124a710f6a37aa143b081923a
gabriel <augustosilva@ua.pt>
2018-11-03 16:12:27 +0000

    fileblocks: soWriteFileBlock

--- sofs18/src/work_src/work_fileblocks/work_write_fileblock.cpp
+++ sofs18/src/work_src/work_fileblocks/work_write_fileblock.cpp
@@ -7,6 +7,7 @@
 
 #include <string.h>
 #include <inttypes.h>
+#include <stdio.h>
 
 namespace sofs18
 {
@@ -15,10 +16,20 @@ namespace sofs18
 
         void soWriteFileBlock(int ih, uint32_t fbn, void *buf)
         {
-            soProbe(332, "%s(%d, %u, %p)\n", __FUNCTION__, ih, fbn, buf);
+           	soProbe(332, "%s(%d, %u, %p)\n", __FUNCTION__, ih, fbn, buf);
+
+			uint32_t block_number = sofs18::soGetFileBlock(ih, fbn);
+	
+			//if the file block is nil we need to allocate it
+			if(block_number == NullReference){
+		 		block_number = sofs18::soAllocFileBlock(ih, fbn);
+			}
+				
+			printf("BN : %d \n", block_number);
+			sofs18::soWriteDataBlock(block_number, buf);
 
             /* change the following line by your code */
-            bin::soWriteFileBlock(ih, fbn, buf);
+            //bin::soWriteFileBlock(ih, fbn, buf);
         }
 
     };
