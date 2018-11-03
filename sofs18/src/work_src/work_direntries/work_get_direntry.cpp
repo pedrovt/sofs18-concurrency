fe8f2447a2eed8bcc7a3d98494317fd1a1b880ff
gabriel <augustosilva@ua.pt>
2018-11-03 22:47:04 +0000

    freelists:deplete_bicache -> bug fix || direntries:get_direntry -> feito para blocos em endereçamento direto

--- sofs18/src/work_src/work_direntries/work_get_direntry.cpp
+++ sofs18/src/work_src/work_direntries/work_get_direntry.cpp
@@ -17,9 +17,55 @@ namespace sofs18
         uint32_t soGetDirEntry(int pih, const char *name)
         {
             soProbe(201, "%s(%d, %s)\n", __FUNCTION__, pih, name);
+		 	
+			/*checks if the char '/' is in the name in which case
+			an exception is thrown because the name should be a base name*/	
+			if(strchr(name, '/')){
+				throw SOException(EINVAL, __FUNCTION__);
+			}
+			
+			/*check if pih is a directory
+			if not throw an exception*/
+			SOInode* parent_inode = sofs18::soITGetInodePointer(pih);
+			if(!S_ISDIR(parent_inode -> mode)){
+				//perguntar ao stor. o erro nao existe
 
-            /* change the following line by your code */
-            return bin::soGetDirEntry(pih, name);
+				//throw SOException(ENODIR, __FUNCTION__);
+				throw SOException(20, __FUNCTION__);
+			}
+		
+			//search in the directly adressed blocks
+			for(uint32_t w=0; w<N_DIRECT; w++){
+				//read the direntries block
+				char buffer[BlockSize];
+				sofs18::soReadFileBlock(pih,w,buffer);
+				
+				//go through each line
+				for(uint32_t i=0; i<DirentriesPerBlock; i++){
+					
+					//the first 4 bytes are the name of the inode
+					uint32_t inode_number = 0;
+					for(int k=0; k<4; k++){
+						inode_number += buffer[k + i*sizeof(SODirEntry)] << 8*k;
+					}
+
+					//the rest of the bytes are the base name of the link
+					char string[sizeof(SODirEntry) - 4];
+					int index = 0;
+					for(uint32_t j=4; j<sizeof(SODirEntry); j++){
+						string[index] =  buffer[j + i*sizeof(SODirEntry)];
+						index +=1;
+					}
+					//if the same name is found return the number of the inode
+					if(strcmp(string, name) == 0){
+						return inode_number;
+					}	
+				
+				}
+			}
+						
+			//return NullReference if we didn't find the dir entry
+			return NullReference;
         }
 
     };
