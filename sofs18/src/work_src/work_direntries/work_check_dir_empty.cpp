3d36bb637a386eeeadc83e79e77fda5e7cdd1a70
dmtar <dmtar@ua.pt>
2018-11-04 19:01:53 +0000

    [direntries/checkemptiness] Already tested.

--- sofs18/src/work_src/work_direntries/work_check_dir_empty.cpp
+++ sofs18/src/work_src/work_direntries/work_check_dir_empty.cpp
@@ -9,20 +9,23 @@
 #include <string.h>
 #include <sys/stat.h>
 
-namespace sofs18
-{
-    namespace work
-    {
-
-        bool soCheckDirEmpty(int ih)
-        {
+namespace sofs18{
+    namespace work{
+        bool soCheckDirEmpty(int ih){
             soProbe(205, "%s(%d)\n", __FUNCTION__, ih);
 
             /* change the following line by your code */
-            return bin::soCheckDirEmpty(ih);
+            SODirEntry blockDirEntries[DirentriesPerBlock];				// array to store entries of a block
+            sofs18::soReadFileBlock(ih, i, blockDirEntries);			// read block to the array
+            for (uint32_t t = 0; t < DirentriesPerBlock; t++) {			// cycling each dirEntry
+                targetDirEntry = blockDirEntries[i];
+                if (strcmp(".", targetDirEntry.name) == 0 or strcmp("..", targetDirEntry.name) == 0){	// targetDirEntry should be either "." or ".."
+                    return false;               														
+                }	
+            }
+            return true;
+            //return bin::soCheckDirEmpty(ih);
         }
-
     };
-
 };
 
